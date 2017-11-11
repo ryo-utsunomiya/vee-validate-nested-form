74f495ef0978c5dbd25b7293f578b63c06d7349f
Ryo Utsunomiya <millionsnowliving@gmail.com>
2017-11-12 00:19:54 +0900

    basic functionality

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,27 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld/>
+    <entry-form/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
+  import EntryForm from './components/EntryForm';
 
-export default {
-  name: 'app',
-  components: {
-    HelloWorld,
-  },
-};
+  export default {
+    name: 'app',
+    components: {
+      EntryForm,
+    },
+  };
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
 </style>
---
+++ src/components/EntryForm.vue
@@ -0,0 +1,62 @@
+<template>
+  <form action="">
+    <div>
+      <label for="name">Name</label>
+      <input type="text"
+             name="name"
+             id="name"
+             v-model="name">
+    </div>
+    <div>
+      <label for="email">Email</label>
+      <input type="email"
+             name="email"
+             id="email"
+             v-model="email">
+    </div>
+    <div>
+      <label for="tel">Tel</label>
+      <input type="tel"
+             name="tel"
+             id="tel"
+             v-model="tel">
+    </div>
+    <div>
+      <label for="address">Address</label>
+      <input type="text"
+             name="address"
+             id="address"
+             v-model="address">
+    </div>
+    <div>
+      <label for="shipToDifferentAddress">Ship to different address</label>
+      <input type="checkbox"
+             name="shipToDifferentAddress"
+             v-model="shipToDifferentAddress"
+             id="shipToDifferentAddress">
+    </div>
+    <div v-show="shipToDifferentAddress">
+      <label for="shippingAddress">Shipping Address</label>
+      <input type="text"
+             name="address"
+             id="shippingAddress"
+             v-model="shippingAddress">
+    </div>
+    <input type="submit" value="submit">
+  </form>
+</template>
+<script>
+  export default {
+    name: 'entry-form',
+    data() {
+      return {
+        name: '',
+        email: '',
+        tel: '',
+        address: '',
+        shipToDifferentAddress: false,
+        shippingAddress: '',
+      };
+    },
+  };
+</script>
